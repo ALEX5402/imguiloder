0ed3493a3cca92468ff076eb735732585a7df11c
Alex <alex5402private@gmail.com>
2024-04-04 10:00:27 +0530

    Delete main.py

--- app/src/main/jniLibs/arm64-v8a/main.py
+++
@@ -1,7 +0,0 @@
-import os
-import lief
-
-#readelf -d libmmkvopp.so // to check lib included or not
-libnative = lief.parse("libpine.so")#main target lib you want to include
-libnative.add_library("libcrashlog.so") #which lib you want to include
-libnative.write("libpine-mod.so") #final output lib
