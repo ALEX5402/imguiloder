2037f29a5f23f8d986465e8941916c9d4b99a8da
Alex <alex5402private@gmail.com>
2024-04-09 22:23:31 +0530

    added load rule

--- app/src/main/java/com/alex/mmop/app.kt
+++ app/src/main/java/com/alex/mmop/app.kt
@@ -2,16 +2,25 @@ package com.alex.mmop
 
 import android.app.Application
 import android.content.Context
+import com.alex.mmop.api.LOGS
 import com.fvbox.lib.FCore
-import com.google.firebase.FirebaseApp
-import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.fvbox.lib.rule.common.PackageRule
 
 class app : Application() {
+    val packagenames = listOf(
+        "com.pubg.imobile",
+        "com.tencent.ig",
+        "com.pubg.korea",
+        "com.tencent.tmgp.pubgmhd",
+        "com.rekoo.pubgm",
+        "com.vng.pubgmobile"
+    )
 
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
 
          FCore.get().init(this,true)
+
     }
 
     override fun onCreate() {
@@ -20,6 +29,28 @@ class app : Application() {
      //   FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true)
         if(FCore.isClient()) {
             return
+        }
+        loadrule(packagenames)
+    }
+
+    fun loadrule ( packagenaems : List<String>){
+        try {
+            for (packagename in packagenaems ){
+                val isinstalled =   FCore.get().isInstalled(packagename, 0)
+                if ( isinstalled){
+                    val fapplicationinfo = FCore.get().getApplicationInfo(packagename,0,0) ?: continue
+
+                    val builder = PackageRule.Builder(fapplicationinfo.packageName).addUserId(0)
+                    builder.setAllowSystemInteraction(true)
+                    builder.isHideRoot(true)
+                    builder.isDisableKill(true)
+                    FCore.get().addRule(builder.build())
+                    LOGS.info("Load Rule: " + fapplicationinfo.packageName)
+                }
+            }
+
+        }catch ( err : Exception ){
+            err.printStackTrace()
         }
     }
 }
